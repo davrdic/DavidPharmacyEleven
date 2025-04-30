1867f4be31182234dc0db8501dac59b2fd24f58e
David <davrdic@gmail.com>
2025-04-30 14:46:00 +0000

    Cleaned mainwindow.

--- UI/MainWindow/mainwindow.cpp
+++ UI/MainWindow/mainwindow.cpp
@@ -2,23 +2,25 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     setWindowTitle("Dave's Pharmacy Management System");
 
-    // Create Windows
+    // Initialize and add sub-windows to the stacked widget
     manageDoctorsWindow = new ManageDoctorsWindow(this);
     ui->stackedWidget->addWidget(manageDoctorsWindow);
 
     manageCustomersWindow = new ManageCustomersWindow(this);
     ui->stackedWidget->addWidget(manageCustomersWindow);
 
-    // Create connections between windows
+    // Connect main buttons to their handlers
     connect(ui->manageDoctorsButton, &QPushButton::clicked, this, &MainWindow::on_manageDoctorsButton_clicked);
-    connect(manageDoctorsWindow, &ManageDoctorsWindow::backClicked, this, &MainWindow::on_BackToMainClicked);
+    connect(ui->manageCustomersButton, &QPushButton::clicked, this, &MainWindow::on_manageCustomersButton_clicked);
 
+    // Connect back signals from sub-windows to return to main view
+    connect(manageDoctorsWindow, &ManageDoctorsWindow::backClicked, this, &MainWindow::on_BackToMainClicked);
     connect(manageCustomersWindow, &ManageCustomersWindow::backClicked, this, &MainWindow::on_BackToMainClicked);
 }
 
@@ -33,6 +35,12 @@ void MainWindow::on_manageDoctorsButton_clicked()
     ui->stackedWidget->setCurrentWidget(manageDoctorsWindow);
 }
 
+void MainWindow::on_manageCustomersButton_clicked()
+{
+    setWindowTitle("Manage Customers");
+    ui->stackedWidget->setCurrentWidget(manageCustomersWindow);
+}
+
 void MainWindow::on_BackToMainClicked()
 {
     ui->stackedWidget->setCurrentIndex(0);
@@ -41,12 +49,6 @@ void MainWindow::on_BackToMainClicked()
 
 void MainWindow::on_doctorsBackButton_clicked()
 {
+    // Appears redundant; included in case it's connected elsewhere.
     ui->stackedWidget->setCurrentIndex(0);
 }
-
-void MainWindow::on_manageCustomersButton_clicked()
-{
-    setWindowTitle("Manage Customers");
-    ui->stackedWidget->setCurrentWidget(manageCustomersWindow);
-}
-
--- UI/MainWindow/mainwindow.h
+++ UI/MainWindow/mainwindow.h
@@ -11,21 +11,48 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
+/**
+ * @brief The MainWindow class represents the main navigation window of the application.
+ * It contains navigation to manage doctors and customers.
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructs the MainWindow.
+     * @param parent Optional QWidget parent.
+     */
     MainWindow(QWidget *parent = nullptr);
+
+    /**
+     * @brief Destructor for MainWindow.
+     */
     ~MainWindow();
 
 private slots:
+    /**
+     * @brief Slot triggered when the "Manage Doctors" button is clicked.
+     * Switches to the ManageDoctorsWindow.
+     */
     void on_manageDoctorsButton_clicked();
 
-    void on_doctorsBackButton_clicked();
-
+    /**
+     * @brief Slot triggered when the "Back" button is clicked in any sub-window.
+     * Returns to the main screen.
+     */
     void on_BackToMainClicked();
 
+    /**
+     * @brief (Legacy/unused?) Slot for returning from doctors page to main. Kept for compatibility.
+     */
+    void on_doctorsBackButton_clicked();
+
+    /**
+     * @brief Slot triggered when the "Manage Customers" button is clicked.
+     * Switches to the ManageCustomersWindow.
+     */
     void on_manageCustomersButton_clicked();
 
 private:
@@ -33,4 +60,5 @@ private:
     ManageDoctorsWindow *manageDoctorsWindow;
     ManageCustomersWindow *manageCustomersWindow;
 };
+
 #endif // MAINWINDOW_H
